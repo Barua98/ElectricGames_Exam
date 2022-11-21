2255ae69c07ef54224bd034fa0c152a63a17ee31
TlomMoco <94906664+TlomMoco@users.noreply.github.com>
2022-11-21 20:38:30 +0100

    service added

---
+++ electricgames/src/services/ElectricGamesService.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const ElectricGamesService = (() => {
+
+    const electricGamesEndpoints = {
+        games: "",
+        gameCharacters: ""
+    }
+
+    const getAll = async () => {
+
+    }
+
+    return{
+
+    }
+
+})();
+
+export default ElectricGamesService;
