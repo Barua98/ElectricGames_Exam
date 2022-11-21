e698b8448563b4efccd3b39bb87ed7d45fb50a9b
TlomMoco <94906664+TlomMoco@users.noreply.github.com>
2022-11-21 20:34:30 +0100

    context added

---
+++ electricgames/src/context/GameContext.tsx
@@ -0,0 +1,13 @@
+import { ReactNode, useEffect, useState, createContext } from "react"
+
+type Props = {
+    children: ReactNode
+}
+
+const GameProvider = ({children} : Props) => {
+
+
+
+}
+
+export default GameProvider;
