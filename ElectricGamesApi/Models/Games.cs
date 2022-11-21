e778f92327d3dd4a1412cf28cf9c8dd27f887133
Barua98 <ronyb98@gmail.com>
2022-11-21 21:15:05 +0100

    odfied

--- ElectricGamesApi/Models/Games.cs
+++ ElectricGamesApi/Models/Games.cs
@@ -8,8 +8,8 @@ public class Game : IGame
 {
     [Key]
     public int Id {get; set;}
-    public string? Title {get; set;}
-    public string? Image {get; set;}
+    public string Title {get; set;}
+    public string Image {get; set;}
     public string Description {get; set;}
     public string Platform {get; set;}
     public int ReleaseYear {get; set;}
