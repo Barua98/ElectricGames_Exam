2781dffc6af61eef6db612efd1f5bbd618f8e437
TlomMoco <94906664+TlomMoco@users.noreply.github.com>
2022-11-22 13:15:00 +0100

    endpoints added

--- electricgames/src/services/ElectricGamesService.ts
+++ electricgames/src/services/ElectricGamesService.ts
@@ -3,16 +3,23 @@ import axios from "axios";
 const ElectricGamesService = (() => {
 
     const electricGamesEndpoints = {
-        games: "",
-        gameCharacters: ""
+        games: "https://localhost:7016",
+        gameCharacters: "https://localhost:7016"
     }
 
-    const getAll = async () => {
+    const getAllGames = async () => {
+        const result = await axios.get(electricGamesEndpoints.games);
+        return result.data;
+    }
 
+    const getAllGameCharacters = async () => {
+        const result = await axios.get(electricGamesEndpoints.gameCharacters);
+        return result.data;
     }
 
     return{
-
+        getAllGames,
+        getAllGameCharacters
     }
 
 })();
