f1d118ffbef823aa00e20cf1f27c1da5a7755d98
Barua98 <ronyb98@gmail.com>
2022-11-22 13:09:15 +0100

    modified

---
+++ ElectricGamesApi/Controllers/GameController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc; 
+using Microsoft.EntityFrameworkCore; 
+using ElectricGamesApi.Contexts; 
+using ElectricGamesApi.Models; 
+
+namespace ElectricGamesApi.Controllers; 
+
+[ApiController]
+[Route("[controller]")]
+
+public class GameController : ControllerBase
+{
+    private readonly GameContext context;
+
+    public GameController(GameContext _context)
+    {
+        context = _context; 
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Game>>> Get()
+    {
+        try
+        {
+           List<Game> games = await context.Game.ToListAsync(); 
+           return Ok(games); 
+        }
+        catch
+        {
+            return StatusCode(500); 
+        }
+    }
+
+}
