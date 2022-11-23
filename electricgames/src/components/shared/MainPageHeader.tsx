c103fe063a035122e81e6ce7a9d1f629e237d0f1
TlomMoco <94906664+TlomMoco@users.noreply.github.com>
2022-11-23 20:17:15 +0100

    modified

--- electricgames/src/components/shared/MainPageHeader.tsx
+++ electricgames/src/components/shared/MainPageHeader.tsx
@@ -7,9 +7,9 @@ const MainPageHeader = () => {
             <h1>Her kan vi ha en logo</h1>
             <ul>
                 <li><Link to="/">Home</Link></li>
-                <li><Link to="/delete-games">Delete games & characters</Link></li>
-                <li><Link to="/update-games">Update games & characters</Link></li>
                 <li><Link to="/search">Search page</Link></li>
+                <li><Link to="/update-games">Update games & characters</Link></li>
+                <li><Link to="/delete-games">Delete games & characters</Link></li>
             </ul>
 
         </header>
